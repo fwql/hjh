f4568d61aadffcac9fc3caf660e08e48730d6b7c
hjh <1516710750@qq.com>
2021-01-19 23:52:35 +0800

    添加成功

---
+++ src/main/java/com/itheima/service/DemoService.java
@@ -0,0 +1,9 @@
+package com.itheima.service;
+
+import java.io.PrintStream;
+
+public class DemoService {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈，成功了");
+    }
+}
